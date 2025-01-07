884ca1c2e28ca4037b79c59dea52b164f1e9ee94
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2025-01-07 11:54:56 +0100

    Daten aus CMS Directus anzeigen fertig

--- src/components/PeopleContainer.tsx
+++ src/components/PeopleContainer.tsx
@@ -1,16 +1,41 @@
 import Card from './Card';
+import { useEffect, useState } from "react";
 
 
+type Props = {};
+type Person = {
+    name: string;
+    description: string;
+    Image: string;
+}
 
-export default function PeopleContainer() {
+export default function PeopleContainer({ }: Props) {
+    const [people, setPeople] = useState<Person[]>([]);
+
+    useEffect(() => {
+        fetch("http://10.115.1.33:8055/items/people")
+            .then((response) => response.json().then((data: any) => {
+                setPeople(data.data);
+
+            })
+            );
+    }, []);
+    console.log("people", people);
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 justify-center bg-green-900 p-6 gap-4">
-            <Card name="Diego Zechner" description="Lorem ipsum dolor sit amet." imageUrl="https://picsum.photos/200/300" />
-            <Card name="Person 2" description="Lorem ipsum dolor sit amet." imageUrl="https://picsum.photos/200/300" />
-            <Card name="Person 3" description="Lorem ipsum dolor sit amet." imageUrl="https://picsum.photos/200/300" />
-            <Card name="Person 4" description="Lorem ipsum dolor sit amet." imageUrl="https://picsum.photos/200/300" />
-            <Card name="Person 5" description="Lorem ipsum dolor sit amet." imageUrl="https://picsum.photos/200/300" />
-            <Card name="Person 6" description="Lorem ipsum dolor sit amet." imageUrl="https://picsum.photos/200/300" />
+            {people.map((person: Person) => {
+                return (
+
+                    <Card
+                        name={person.name}
+                        description={person.description}
+                        imageUrl={
+                            "http://10.115.1.33:8055/assets/" + person.Image
+                        } />
+                );
+            })}
         </div>
+
     );
 }
